938e120897b42056fa83c683c9324ea4c6a5e54a
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-04-22 20:23:34 -0500

    Listo el modelo de la BdD en Python, hare la migra

--- project_news/home/models.py
+++ project_news/home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    SOURCES = {
+        ("MD","Medium"),
+        ("GH","Github Trends"),
+        ("RD","Reddit"),
+        ("NY","NewY ork Times"),
+    }
+    idArticle = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    summary = models.TextField()
+    articleDate = models.DateTimeField()
+    source = models.CharField(max_length=30,choices=SOURCES)
+    creationDate = models.DateTimeField(auto_now=True)
