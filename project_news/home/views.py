4294bbc1ac1c9e7e55d46622750914a11ce4a6ae
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-04-24 00:13:08 -0500

    Parte del API de nytimes

--- project_news/home/views.py
+++ project_news/home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+import requests
+from .forms import SubmitEmbed
+from .serializer import EmbedSerializer
 
 # Create your views here.
 
 def index(request):
     return render(request,"home/index.html")
+
+def getnews(request):
+    #r = request.get('https://api.nytimes.com/svc/topstories/v2/home.json', {'api-key': '7d6470dfcd954000af4d0d8927b8f2d7'})
+    #j = r.json()
+    #return j
+    return render(request,"home/getnews.html")
